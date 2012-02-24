604a53067165f19413928c8998066377c9f74beb
JessThrysoee <jess@thrysoee.dk>
2012-02-25 00:17:21 +0100

    refactored clock synchronization

--- getdata.js
+++ getdata.js
@@ -1,7 +1,7 @@
 /*jshint bitwise:false*/
 
 function parseCSV(csv, threshold) {
-   var i, l, lines, fields, metrics = [];
+   var i, l, lines, fields, elapsedCorrection, metrics = [];
    lines = csv.split('\n');
 
    for (i = 1, l = lines.length - 1; i < l; i++) {
@@ -11,22 +11,25 @@ function parseCSV(csv, threshold) {
       if (+fields[8] === -1 || +fields[9] === -1 || +fields[10] === -1 || +fields[11] === -1) {
          continue;
       }
-      
-     // ignore request with elapsed time less than <threshold> ms
-     if (+fields[5] < threshold) {
-        continue;
-     }
+
+      //elapsedCorrection = +fields[11];
+      elapsedCorrection = 0;
+
+      // ignore request with elapsed time less than <threshold> ms
+      if (+fields[5] - elapsedCorrection < threshold) {
+         continue;
+      }
 
       metrics.push({
          'title': fields[1],
          'timestamp': +fields[4],
-         'elapsed': +fields[5],
+         'elapsed': +fields[5] - elapsedCorrection,
          'webserver-nearside-timestamp': +fields[6],
-         'webserver-nearside-elapsed': +fields[7],
+         'webserver-nearside-elapsed': +fields[7] - elapsedCorrection,
          'webserver-farside-timestamp': +fields[8],
-         'webserver-farside-elapsed': +fields[9],
+         'webserver-farside-elapsed': +fields[9] - elapsedCorrection,
          'server-timestamp': +fields[10],
-         'server-elapsed': +fields[11],
+         'server-elapsed': +fields[11] - elapsedCorrection,
          'tx': +fields[2],
          'rx': +fields[3],
          'client-webserver-offset': null,
@@ -42,70 +45,39 @@ function parseCSV(csv, threshold) {
 }
 
 function addWebserverToServerOffset(metrics) {
-   var i, l, m, hash, buckets;
-
-   buckets = {};
-
-   //for synchronized clocks where request and response times are equal, the following it true:
-   //   m.timestamp + (m.elapsed - m['webserver-nearside-elapsed']) / 2 = m['webserver-nearside-timestamp'];
-   function calcOffsets(m) {
-      return {
-         elapsed: m['webserver-farside-elapsed'],
-         'webserver-server-offset': ~~ (m['webserver-farside-timestamp'] + (m['webserver-farside-elapsed'] - m['server-elapsed']) / 2 - m['server-timestamp'])
-      };
-   }
-
-   function calcHash(m) {
-      var interval = 1000 * 600; // 10 min buckets
-      return ~~ (m['webserver-farside-timestamp'] / interval);
-   }
-
-   function addOffsetsTo(metric) {
-      var offsets = buckets[calcHash(metric)];
-      metric['webserver-server-offset'] = offsets['webserver-server-offset'];
-   }
-
-   for (i = 0, l = metrics.length; i < l; i++) {
-      m = metrics[i];
-
-      hash = calcHash(m);
-      if (buckets[hash]) {
-         if (m['webserver-farside-elapsed'] < buckets[hash].elapsed) {
-            buckets[hash] = calcOffsets(m);
-         }
-      } else {
-         buckets[hash] = calcOffsets(m);
-      }
-   }
-
-   for (i = 0, l = metrics.length; i < l; i++) {
-      addOffsetsTo(metrics[i]);
-   }
+   addOffsetsTo(metrics, 'webserver-server-offset', 'webserver-farside-timestamp', 'webserver-farside-elapsed', 'server-timestamp', 'server-elapsed');
 }
 
 
 function addClientToWebserverOffset(metrics) {
+   addOffsetsTo(metrics, 'client-webserver-offset', 'timestamp', 'elapsed', 'webserver-nearside-timestamp', 'webserver-nearside-elapsed');
+}
+
+
+function addOffsetsTo(metrics, property, fromTimestamp, fromElapsed, toTimestamp, toElapsed) {
    var i, l, m, hash, buckets;
 
    buckets = {};
 
    //for synchronized clocks where request and response times are equal, the following it true:
-   //   m.timestamp + (m.elapsed - m['webserver-nearside-elapsed']) / 2 = m['webserver-nearside-timestamp'];
+   //   m[fromTimestamp] + (m[fromElapsed] - m[toElapsed]) / 2 =  m[toTimestamp];
    function calcOffsets(m) {
-      return {
-         elapsed: m.elapsed,
-         'client-webserver-offset': ~~ (m.timestamp + (m.elapsed - m['webserver-nearside-elapsed']) / 2 - m['webserver-nearside-timestamp']),
-      };
+      var o = {};
+
+      o.elapsed = m[fromElapsed];
+      o[property] = ~~ (m[fromTimestamp] + (m[fromElapsed] - m[toElapsed]) / 2 - m[toTimestamp]);
+
+      return o;
    }
 
    function calcHash(m) {
       var interval = 1000 * 600; // 10 min buckets
-      return ~~ (m.timestamp / interval);
+      return ~~ (m[fromTimestamp] / interval);
    }
 
-   function addOffsetsTo(metric) {
+   function addTo(metric) {
       var offsets = buckets[calcHash(metric)];
-      metric['client-webserver-offset'] = offsets['client-webserver-offset'];
+      metric[property] = offsets[property];
    }
 
    for (i = 0, l = metrics.length; i < l; i++) {
@@ -113,7 +85,7 @@ function addClientToWebserverOffset(metrics) {
 
       hash = calcHash(m);
       if (buckets[hash]) {
-         if (m.elapsed < buckets[hash].elapsed) {
+         if (m[fromElapsed] < buckets[hash].elapsed) {
             buckets[hash] = calcOffsets(m);
          }
       } else {
@@ -122,6 +94,6 @@ function addClientToWebserverOffset(metrics) {
    }
 
    for (i = 0, l = metrics.length; i < l; i++) {
-      addOffsetsTo(metrics[i]);
+      addTo(metrics[i]);
    }
 }
